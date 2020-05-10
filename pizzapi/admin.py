869d4e60c7c501fc42a7683557585b7bffd8bf48
Tolulope Adetula <31691737+Tooluloope@users.noreply.github.com>
2020-05-10 05:16:48 +0100

    admin error

--- pizzapi/admin.py
+++ pizzapi/admin.py
@@ -5,4 +5,5 @@ from .models import Pizza,Order, UserProfile
 # Register your models here.
 admin.site.register(Pizza)
 admin.site.register(Order)
+admin.site.register(UserProfile)
 
--- pizzapi/models.py
+++ pizzapi/models.py
@@ -66,7 +66,10 @@ class UserProfile(AbstractBaseUser):
     fullname = models.CharField(max_length = 50, blank=False, null=False)
     username = models.CharField(max_length=60, unique=True,blank=False, null=False)
     email = models.EmailField(max_length=254, unique=True,blank=False, null=False)
-
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     
