6aca9f70c2b303399919a2e05de14c3530eed9d6
Tolulope Adetula <31691737+Tooluloope@users.noreply.github.com>
2020-05-12 13:51:47 +0100

    uuid

--- pizzapi/models.py
+++ pizzapi/models.py
@@ -16,7 +16,7 @@ class Pizza(models.Model):
 
 
 class Order(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=60)
     email = models.CharField(max_length=150)
     address = models.CharField(max_length=150)
