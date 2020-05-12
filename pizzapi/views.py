843e14934b93fa69a0f99acb8b41d85f3be3ae78
Tolulope Adetula <31691737+Tooluloope@users.noreply.github.com>
2020-05-12 07:03:13 +0100

    Descending order

--- pizzapi/views.py
+++ pizzapi/views.py
@@ -25,7 +25,7 @@ class OrderViewSet(viewsets.ModelViewSet):
 
     serializer_class = OrderSerializer
 
-    queryset = Order.objects.all().order_by('created_at')
+    queryset = Order.objects.all().order_by('-created_at')
 
 
 class UserAPIView(generics.RetrieveAPIView):
